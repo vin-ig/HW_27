07a5d12cf7504296d3ca40a09863c522d4856f31
Harry <vin-ig@yandex.ru>
2022-08-13 08:11:52 +0300

    POST request completed

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -34,6 +36,31 @@ class AdView(ListView):
 			})
 		return JsonResponse(result, safe=False)
 
+	def post(self, request):
+		data = json.loads(request.body)
+
+		ad = Ad()
+
+		ad.name = data.get('name')
+		ad.author = data.get('author')
+		ad.price = data.get('price')
+		ad.description = data.get('description')
+		ad.address = data.get('address')
+		ad.is_published = data.get('is_published')
+
+		ad.save()
+
+		return JsonResponse({
+				'id': ad.id,
+				'name': ad.name,
+				'author': ad.author,
+				'price': ad.price,
+				'description': ad.description,
+				'address': ad.address,
+				'is_published': ad.is_published,
+
+		}, safe=False)
+
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CategoryView(ListView):
@@ -50,6 +77,19 @@ class CategoryView(ListView):
 				'name': elem.name,
 			})
 		return JsonResponse(result, safe=False)
+
+	def post(self, request):
+		data = json.loads(request.body)
+
+		category = Category()
+		category.name = data.get('name')
+
+		category.save()
+
+		return JsonResponse({
+				'id': category.id,
+				'name': category.name,
+			}, safe=False)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
